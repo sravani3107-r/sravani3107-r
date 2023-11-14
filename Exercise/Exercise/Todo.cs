00da0d3a7a9e7abeccf7d8e7cad56dd2c55c02d3
Sravani Rachakonda <145092759+sravani3107-r@users.noreply.github.com>
2023-11-14 14:07:59 -0600

    Api project

---
+++ Exercise/Exercise/Todo.cs
@@ -0,0 +1,10 @@
+﻿public class Todo
+{
+    public int Id { get; set; }
+    public string? Question { get; set; }
+    public string? Correctanswer { get; set; }
+    public string? A1 { get; set; }
+    public string? B1 { get; set; }
+    //public List<string>? IncorrectAnswers { get; set; }
+    public bool IsComplete { get; set; }
+}
